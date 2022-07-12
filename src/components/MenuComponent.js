9271934028306b17339a32bf0b8b0248da005415
Pham Ho Phi <phamhophi05@gmail.com>
2022-07-12 23:43:19 +0700

    an

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,71 +1,30 @@
 import React, { Component } from "react";
-import { Media } from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardText, CardTitle } from "reactstrap";
 
 class Menu extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      dishes: [
-        {
-          id: 0,
-          name: "Uthappizza",
-          image: "assets/images/uthappizza.png",
-          category: "mains",
-          label: "Hot",
-          price: "4.99",
-          description:
-            "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
-        },
-        {
-          id: 1,
-          name: "Zucchipakoda",
-          image: "assets/images/zucchipakoda.png",
-          category: "appetizer",
-          label: "",
-          price: "1.99",
-          description:
-            "Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce",
-        },
-        {
-          id: 2,
-          name: "Vadonut",
-          image: "assets/images/vadonut.png",
-          category: "appetizer",
-          label: "New",
-          price: "1.99",
-          description:
-            "A quintessential ConFusion experience, is it a vada or is it a donut?",
-        },
-        {
-          id: 3,
-          name: "ElaiCheese Cake",
-          image: "assets/images/elaicheesecake.png",
-          category: "dessert",
-          label: "",
-          price: "2.99",
-          description:
-            "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
-        },
-      ],
-    };
+    this.state = {};
   }
 
   render() {
-    const menu = this.state.dishes.map((dish) => {
+    const menu = this.props.dishes.map((dish) => {
       return (
-        <div key={dish.id} className="col-12 mt-5">
-          <Media tag="li">
-            <Media left middle>
-              {/* Hiển thị hình ảnh món ăn */}
-              <Media object src={dish.image} alt={dish.name}></Media>
-            </Media>
-            <Media body className="ml-5">
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+          <Card tag="li">
+            {/* Hiển thị hình ảnh món ăn */}
+            <CardImg
+              width="100%"
+              object
+              src={dish.image}
+              alt={dish.name}
+            ></CardImg>
+
+            <CardImgOverlay>
               {/* Hiển thị tên món ăn*/}
-              <Media heading>{dish.name}</Media>
-              {/* Mô tả món ăn */}
-              <p>{dish.description}</p>
-            </Media>
-          </Media>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
         </div>
       );
     });
